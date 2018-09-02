d228aec714bcb2bd8e2d564ee19d02e650a396d8
rjbertille <rjbertille@gmail.com>
2018-09-02 18:18:30 +0200

    exercise file

---
+++ script.js
@@ -0,0 +1,11 @@
+const s = [5, 7, 2];
+
+function editInPlace() {
+  "use strict";
+  // change code below this line
+s.unshift(s.pop());
+  // s = [2, 5, 7]; <- this is invalid
+
+  // change code above this line
+}
+editInPlace();
